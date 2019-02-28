dffb6b6123436447c68f1b2a05f91c439fe586fd
Vincent Mustillo <vjmustillo@gmail.com>
2019-02-27 22:48:23 -0500

    Created a simple server using express and started to create the GraphQL schema

---
+++ server/schema/schema.js
@@ -0,0 +1,22 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType } = graphql;
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { GraphQLString }
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString } }
+        }
+    }
+});
