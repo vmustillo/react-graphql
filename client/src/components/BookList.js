91ab91735f1c780356c294328389e2003e434c86
Vincent Mustillo <vjmustillo@gmail.com>
2019-03-13 23:32:35 -0400

    Dynamically returns all the books from the db in a list and displays.  The graphql books result set is mapped to html list elements

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -12,13 +12,20 @@ const getBooksQuery = gql`
 `;
 
 class BookList extends Component {
+  displayBooks() {
+    var data = this.props.data;
+    if (data.loading) {
+      return <div>Loading books...</div>;
+    } else {
+      return data.books.map(book => {
+        return <li key={book.id}>{book.name}</li>;
+      });
+    }
+  }
   render() {
-    console.log(this.props);
     return (
       <div id="main">
-        <ul id="book-list">
-          <li>Book name</li>
-        </ul>
+        <ul id="book-list">{this.displayBooks()}</ul>
       </div>
     );
   }
