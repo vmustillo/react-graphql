ee5411b3e6ca2e3511db9eae100b352b38afe231
Vincent Mustillo <vjmustillo@gmail.com>
2019-03-13 20:18:49 -0400

    Created react frontend using create-react-app in client folder. Created single page with just one component: BookList. Used gql to create and bind a graphql query to the component and was able to successfully log the list of books from the db.  Installed cors and used to allow the client to communicate with the server

--- server/app.js
+++ server/app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 const OPTS = { useNewUrlParser: true };
 mongoose.connect(process.env.URI, OPTS, err => {
